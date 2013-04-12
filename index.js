4b8d91a7515df7cd707af01cf9ad3cc2632302a3
Sergey Antoninko <lyxsus@gmail.com>
2013-04-12 22:39:46 +0400

    dev

--- index.js
+++ index.js
@@ -29,6 +29,14 @@ function promiseRequest (options) {
 		options.method = 'get';
 	}
 
+	if (!options.headers) {
+		options.headers = {};
+	}
+
+	if (!options.headers ['accept-encoding']) {
+		options.headers ['accept-encoding'] = 'gzip, deflate';
+	}
+
 	if (options.sessionId && (options.method == 'PUT' || options.method == 'POST')) {
 		if (options.url.indexOf ('?') == -1) {
 			options.url += '?sessid=' + options.sessionId;
